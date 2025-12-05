3fba6fc9039fac992f022573322049c0e1457ab4
EdwardJoke <1na-bsftklqsx2@dingtalk.com>
2025-12-05 21:14:16 +0800

    v0.3.0-dev2: Update `build.sh` and `build.zsh` with right version num

--- build/build.sh
+++ build/build.sh
@@ -3,6 +3,6 @@ cargo clean
 cargo build --release
 cd target/release
 
-version=$(cargo metadata --format-version 1 | jq -r '.packages[0].version')
+version=$(cargo pkgid | cut -d '@' -f 2)
 
 tar -czf lox-darwin-amd64-v$version.tar.gz lox
